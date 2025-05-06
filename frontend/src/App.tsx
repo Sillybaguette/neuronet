82d61bdf05220e63666e37a25d64de20682c2cea
Sillybaguette <128024481+Sillybaguette@users.noreply.github.com>
2025-05-06 19:55:50 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import HomeView from "./views/HomeView";
 import EvaluationView from "./views/EvaluationView";
 import ChatView from "./views/ChatView";
