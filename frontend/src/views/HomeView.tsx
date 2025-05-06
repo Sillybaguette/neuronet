a7bf40b989dd7facaa26aaf9740a7da59229dc19
Sillybaguette <128024481+Sillybaguette@users.noreply.github.com>
2025-05-06 19:49:34 +0200

    Update HomeView.tsx

--- frontend/src/views/HomeView.tsx
+++ frontend/src/views/HomeView.tsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeView from "./views/HomeView";
+import EvaluationView from "./views/EvaluationView";
+import ChatView from "./views/ChatView";
 
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/evaluate" element={<EvaluationView />} />
+        <Route path="/chat" element={<ChatView />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
